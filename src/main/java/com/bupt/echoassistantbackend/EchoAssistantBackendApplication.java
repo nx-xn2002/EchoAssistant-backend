55ba8a48bf195ccd0fb981d6dd046209b5bc0bd9
nx-xn2002 <1870230468@qq.com>
2024-07-03 23:34:40 +0800

    init

---
+++ src/main/java/com/bupt/echoassistantbackend/EchoAssistantBackendApplication.java
@@ -0,0 +1,18 @@
+package com.bupt.echoassistantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ *
+ * @author Ni Xiang
+ */
+@SpringBootApplication
+public class EchoAssistantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoAssistantBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bupt/echoassistantbackend/EchoAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.echoassistantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoAssistantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
