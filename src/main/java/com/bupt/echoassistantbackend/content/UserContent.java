df9b76f8aa9ea659e79904e80457fb68463cb8ad
nx-xn2002 <1870230468@qq.com>
2024-07-09 00:20:29 +0800

    调整用户常量

--- src/main/java/com/bupt/echoassistantbackend/content/UserContent.java
+++ src/main/java/com/bupt/echoassistantbackend/content/UserContent.java
@@ -1,15 +1,20 @@
-package com.bupt.echoassistantbackend.common;
+package com.bupt.echoassistantbackend.content;
 
 /**
- * user role
+ * user content
  *
  * @author Ni Xiang
  */
-public class UserRole {
+public interface UserContent {
     /**
      * 用户角色 0 - 管理员, 1 - 学生, 2 - 老师
      */
     public static final int ADMIN = 0;
     public static final int STUDENT = 1;
     public static final int TEACHER = 2;
+
+    /**
+     * 用户登录态
+     */
+    public static final String USER_LOGIN = "USER LOGIN";
 }
